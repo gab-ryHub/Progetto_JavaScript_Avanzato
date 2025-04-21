d991e59a82edffca5d95234ce5de14df79073dfe
gab.ry / Gabriele + Potenza <gabriele.potenza9907@gmail.com>
2025-04-21 20:38:04 +0200

    Altre correzioni. Controlli aggiunti.

--- Functions.js
+++ Functions.js
@@ -3,19 +3,65 @@ import PromptSync from "prompt-sync";
 const prompt = PromptSync();
 
 export function Registra_Atleti(){
+    let nome;
+    let cognome;
+    let data;
+    let id;
+
+    do{
+        nome = prompt("Nome atleta: ");
+        nome = Formatta_Dati(nome);
+    }while(nome == "");
+
+    do{
+        cognome = prompt("Cognome atleta: ");
+        cognome = Formatta_Dati(cognome);
+    }while(cognome == "");
+
+    let giorno, mese, anno;
+    do{
+        console.log("Data di nascita dell'utente.");
+        giorno = prompt("Giorno di nascita: ");
+        mese = prompt("Mese di nascita: ");
+        anno = prompt("Anno di nascita: ");
+        data = giorno + "/" + mese + "/" + anno;
+    }while(giorno == "" || mese == "" || anno == "");
+
+    let controllo;
+    do{
+        id = prompt("Id dell'utente(6 valori alfanumerici): ");
+        id = Formatta_Dati(id);
+        controllo = Check_Id(id);
+    }while(!controllo || id.length > 6);
+
     return{
-        Nome         :  prompt("Nome atleta: "),
-        Cognome      :  prompt("Cognome atleta: "),
-        Data_Nascita :  prompt("Data di nascita dell'utente: "),
-        Id           :  prompt("Id dell'utente: "),
+        Nome         :  nome,
+        Cognome      :  cognome,
+        Data_Nascita :  data,
+        Id           :  id,
     }
 }
 
 export function Registra_Gare(registro_atleti){
-    let atleti_partecipanti = [...Registra_Partecipanti(registro_atleti)];
-    let punteggi_atleti = [...Registra_Punteggi(atleti_partecipanti)];
+    let nome_gara;
+    let descrizione;
 
+    let atleti_partecipanti;
+    let punteggi_atleti;
     let partecipanti = new Array();
+
+    do{
+        nome_gara = prompt("Nome della gara: ");
+        nome_gara = Formatta_Dati(nome_gara);
+    }while(nome_gara == "");
+
+    descrizione = prompt("Dettagli della gara (facoltativi): ");
+
+    if(descrizione.trim() == "")
+        descrizione = "Nessuna descrizione";
+
+    atleti_partecipanti = [...Registra_Partecipanti(registro_atleti)];
+    punteggi_atleti = [...Registra_Punteggi(atleti_partecipanti)];
 
     for(let i in atleti_partecipanti)
         partecipanti[i]={
@@ -24,8 +70,8 @@ export function Registra_Gare(registro_atleti){
             }
 
     return{
-        Nome_Gara   :   prompt("Nome della gara: "),
-        Descrizione :   prompt("Dettagli della gara (facoltativi): "),
+        Nome_Gara   :   nome_gara,
+        Descrizione :   descrizione,
         Partecipanti:   partecipanti,
     }
 }
@@ -54,6 +100,14 @@ export function Calcola_Media(punteggi){
     });
 
     return media;   //restituisce la media dei punteggi in una singola gara
+}
+
+export function Aggiorna_Classifica(registro_gare, classifica_campionato, punteggi){
+    registro_gare.forEach((gare, i) =>{
+        let temp = [...gare.Partecipanti];
+        punteggi[i] =  temp.map(p => p.Punteggi_Atleti);
+    });
+    classifica_campionato.push(...Crea_Classifica(registro_gare));
 }
 
 
@@ -95,12 +149,29 @@ export const Numero_Piazzamenti = function(classifica_campionato, atleta){
     console.log(`Atleta ${atleta}.\n Numero di piazzamenti sul podio: ${dentro}.\n Numero di piazzamenti fuori dal podio: ${fuori}.`);
 }
 
+export const Check_Atleta = (registro_atleti, atleta_ricercato) =>{
+    let controllo = false;
+
+    registro_atleti.forEach(atleta =>{
+        
+        if(atleta.Nome == atleta_ricercato){
+            controllo = true;
+        }
+    })
+    return controllo;
+}
+
+
 
 const Registra_Partecipanti = (registro_atleti) =>{
     let partecipanti = new Array();
     
     registro_atleti.forEach( atleta=>{
-        let partecipa = prompt(`L'atleta ${atleta.Nome} ${atleta.Cognome} è tra i partecipanti della gara (y:n)?`);
+        let partecipa;
+        do{
+            partecipa = prompt(`L'atleta ${atleta.Nome} ${atleta.Cognome} è tra i partecipanti della gara (y:n)? `);
+        }while(partecipa != "y" && partecipa != "n");
+
         if(partecipa == "y")
             partecipanti.push(atleta);
     });
@@ -112,7 +183,31 @@ const Registra_Punteggi = (partecipanti) =>{
     let punteggi = new Array(partecipanti.length);
 
     for(let i = 0; i < partecipanti.length; i++)
-        punteggi[i] = Number.parseInt(prompt(`Punteggio atleta ${partecipanti[i].Nome} ${partecipanti[i].Cognome}:`));
+        do{
+            punteggi[i] = Number.parseInt(prompt(`Punteggio atleta ${partecipanti[i].Nome} ${partecipanti[i].Cognome} (10-100): `));
+        }while(punteggi[i] < 10 || punteggi[i] > 100 || isNaN(punteggi[i]));
 
     return punteggi;
+}
+
+const Formatta_Dati = (dato) =>{
+    return dato.toUpperCase();
+}
+
+const Check_Id = (id) =>{
+    let cont_lettere = 0;
+    let cont_valori = 0;
+    let controllo = false;
+
+    for(let i in id){
+        if(id.charCodeAt(i) >= 65 && id.charCodeAt(i) <=90)
+            cont_lettere++;
+        if(id.charCodeAt(i) >= 48 && id.charCodeAt(i) <= 57)
+            cont_valori++;
+    }
+    if(cont_lettere == 3)
+        if(cont_valori == 3)
+            controllo = true;
+
+    return controllo;
 }
--- Main.js
+++ Main.js
@@ -4,10 +4,10 @@ import * as u from "./Functions.js";
 const prompt = PromptSync();
 
 let scelta;
-let registro_atleti = new Array();
-let registro_gare = new Array();
-let classifica_campionato = new Array();
-let punteggi = new Array();
+let registro_atleti;
+let registro_gare;
+let classifica_campionato;
+let punteggi;
 
 do{
     console.log("\nMenù.\n 1)Registrazione dati anagrafici.\n 2)Registrazione gare e concorrenti.\n 3)Creazione/Aggiornamento classifica.\n 4)Calcolo della media dei punteggi.\n 5)Maggiori info.\n 6)Visualizza registro utenti.\n 7)Uscita.");
@@ -16,11 +16,15 @@ do{
 
     switch(scelta){
         case 1:
-            //aggiungere controllo per verificare se un atleta è già stato iscritto al campionato
             let n_atleti;
 
             if(registro_atleti == null){
-                n_atleti = Number.parseInt(prompt("Numero di atleti partecipanti al campionato:"));
+
+                registro_atleti = new Array();
+                
+                do{
+                    n_atleti = Number.parseInt(prompt("Numero di atleti partecipanti al campionato:"));
+                }while(isNaN(n_atleti) || n_atleti == "0");
 
                 for(let i = 0; i < n_atleti; i++){
                     console.log(`\nAtleta ${i+1} :`);
@@ -28,7 +32,10 @@ do{
                 }
             }
             else{
-                n_atleti = Number.parseInt(prompt("Numero di nuovi atleti da aggiungere al campionato:"));
+                do{
+                    n_atleti = Number.parseInt(prompt("Numero di atleti partecipanti al campionato:"));
+                }while(isNaN(n_atleti) || n_atleti == "0");
+                
                 let atleti_iscritti = registro_atleti.length;
                 
                 for(let i = atleti_iscritti; i < (atleti_iscritti + n_atleti); i++){
@@ -38,79 +45,118 @@ do{
             }
             break;
         case 2:
-            let n_gare;
+            if(registro_atleti != null){
+                let n_gare;
 
-            if(registro_gare == null){
-                n_gare = Number.parseInt(prompt("Numero di gare del campionato: "));
-                
-                
-                for(let i = 0; i < n_gare; i++){
-                    console.log(`\nGara ${i+1} :`);
-                    registro_gare[i] = u.Registra_Gare(registro_atleti);
+                if(registro_gare == null){
+                    
+                    registro_gare = new Array();
+
+                    do{
+                        n_gare= Number.parseInt(prompt("Numero di gare del campionato: "));
+                    }while(isNaN(n_gare) || n_gare == "0");
+                    
+                    
+                    for(let i = 0; i < n_gare; i++){
+                        console.log(`\nGara ${i+1} :`);
+                        registro_gare[i] = u.Registra_Gare(registro_atleti);
+                    }
                 }
-            }
-            else{
-                n_gare = Number.parseInt(prompt("Numero di nuove gare del campionato da registrare: "));
-                let gare_registrate = registro_gare.length;
-                
-                for(let i = gare_registrate; i < (gare_registrate + n_gare); i++){
-                    console.log(`\nGara ${i+1} :`);
-                    registro_gare[i] = u.Registra_Gare(registro_atleti);
+                else{
+                    do{
+                        n_gare= Number.parseInt(prompt("Numero di gare del campionato: "));
+                    }while(isNaN(n_gare) || n_gare == "0");
+                    let gare_registrate = registro_gare.length;
+                    
+                    for(let i = gare_registrate; i < (gare_registrate + n_gare); i++){
+                        console.log(`\nGara ${i+1} :`);
+                        registro_gare[i] = u.Registra_Gare(registro_atleti);
+                    }
                 }
             }
             break;
         case 3:
-            registro_gare.forEach((gare, i) =>{
-                let temp = [...gare.Partecipanti];
-                punteggi[i] =  temp.map(p => p.Punteggi_Atleti);
-            });
-            classifica_campionato.push(...u.Crea_Classifica(registro_gare));
+            if(registro_atleti != null && registro_gare != null){
+                registro_gare.forEach((gare, i) =>{
+                    punteggi = new Array();
+                    let temp = [...gare.Partecipanti];
+                    punteggi[i] =  temp.map(p => p.Punteggi_Atleti);
+                });
+                classifica_campionato.push(...u.Crea_Classifica(registro_gare));
+                console.log("Classifica aggiornata con successo.");
+            }
             break;
         case 4:
-            console.log("\nMedia punteggi per ogni gara: ");
-            u.Calcola_Media(punteggi).forEach((punteggio_gara, index)=>{
-                console.log(`Media punteggi della gara ${index+1}: ${punteggio_gara}`);
-            })
+            if(punteggi != null){
+                console.log("\nMedia punteggi per ogni gara: ");
+                u.Calcola_Media(punteggi).forEach((punteggio_gara, index)=>{
+                    console.log(`Media punteggi della gara ${index+1}: ${punteggio_gara}`);
+                })
+            }
             break;
         case 5:
-            let filtro;
-            do{
-                //aggiungere il controllo che permette di aggiornare automaticamente a classifica se viene aggiunta una gara
-                console.log("\nMenù.\n 1-Percentuale gare vinte.\n 2-Piazzamenti sul podio.\n 3-Numero piazzamenti sul podio.\n 4-Uscita");
-                filtro = Number.parseInt(prompt("Fai una scelta: "));
+            if(registro_atleti != null && registro_gare != null && classifica_campionato != null){
+                let filtro;
+                do{
+                    //aggiungere il controllo che permette di aggiornare automaticamente a classifica se viene aggiunta una gara
+                    console.log("\nMenù.\n 1-Percentuale gare vinte.\n 2-Piazzamenti sul podio.\n 3-Numero piazzamenti sul podio.\n 4-Uscita");
+                    filtro = Number.parseInt(prompt("Fai una scelta: "));
 
-                console.log("\n");
-                
-                switch(filtro){
-                    case 1:
-                        let atleta = prompt("Nome atleta di cui visualizzare le gare vinte: ");
-                        //aggiungere controllo con registro degli atleti
+                    console.log("\n");
+                    
+                    switch(filtro){
+                        case 1:
+                            let atleta;
+                            let ctrl;
 
-                        let vittorie = u.Percentuale_Gare(classifica_campionato, atleta);
-                        console.log(`Numero di gare vinte dall'atleta ${atleta} : ${vittorie}.\nPercentuale sul totale: ${(vittorie/registro_gare.length)*100}%`);
-                        break;
-                    case 2:
-                        let atleta_2 = prompt("Nome atleta di cui visualizzare i piazzamenti sul podio (dentro e fuori) per ogni gara: ");
-                        //aggiungere controllo con registro degli atleti
-                        
-                        console.log(`Posizioni sul podio.\n`);
-                        u.Posizioni_Podio(classifica_campionato, atleta_2);
-                        break;
-                    case 3:
-                        for(let i of registro_atleti){
-                            let atleta = i.Nome;
-                            u.Numero_Piazzamenti(classifica_campionato, atleta);
-                        }
-                        break;
-                    case 4:
-                        console.log("Uscita menù info.");
-                        break;
-                }
-            }while(filtro != 4);
+                            do{
+                                atleta = prompt("Nome atleta di cui visualizzare i piazzamenti sul podio (dentro e fuori) per ogni gara: ");
+                                ctrl = u.Check_Atleta(registro_atleti, atleta);
+                                if(!ctrl)
+                                    console.log("Inserire il nome di un atleta presente nel registro.");
+                            }while(!ctrl);
+
+                            let vittorie = u.Percentuale_Gare(classifica_campionato, atleta);
+                            console.log(`Numero di gare vinte dall'atleta ${atleta} : ${vittorie}.\nPercentuale sul totale: ${(vittorie/registro_gare.length)*100}%`);
+                            break;
+                        case 2:
+                            let atleta_2;
+                            let controllo;
+
+                            do{
+                                atleta_2 = prompt("Nome atleta di cui visualizzare i piazzamenti sul podio (dentro e fuori) per ogni gara: ");
+                                controllo = u.Check_Atleta(registro_atleti, atleta_2);
+                                if(!controllo)
+                                    console.log("Inserire il nome di un atleta presente nel registro.");
+                            }while(!controllo);
+                            
+                            console.log(`Posizioni sul podio.\n`);
+                            u.Posizioni_Podio(classifica_campionato, atleta_2);
+                            break;
+                        case 3:
+                            for(let i of registro_atleti){
+                                let atleta = i.Nome;
+                                u.Numero_Piazzamenti(classifica_campionato, atleta);
+                            }
+                            break;
+                        case 4:
+                            console.log("Uscita menù info.");
+                            break;
+                    }
+                }while(filtro != 4);
+            }
             break;
         case 6:
-            console.log(registro_atleti);
-            console.log(registro_gare.forEach(gara => console.log(gara.Partecipanti)));
+            if(registro_atleti != null && registro_atleti != null){
+                console.log("Atleti Registrati.");
+                registro_atleti.forEach(atleta => console.log(`\nNome: ${atleta.Nome}\nCognome: ${atleta.Cognome}\nData di nascita: ${atleta.Data_Nascita}\nId: ${atleta.Id}`));
+                console.log("\nGare registrare.");
+                registro_gare.forEach((gara, i) => {
+                    console.log(`\nNome gara: ${gara.Nome_Gara} \nDescrizione gara: ${gara.Descrizione}`);
+                    console.log(`\nPartecipanti alla gara: `);
+                    gara.Partecipanti.forEach(atleta => console.log(`\nNome: ${atleta.Atleti_Partecipanti.Nome}\nCognome: ${atleta.Atleti_Partecipanti.Cognome}\nPunteggio: ${atleta.Punteggi_Atleti}`));
+                });
+            }
             break;
         case 7:
             console.log("\nFine programma.");
